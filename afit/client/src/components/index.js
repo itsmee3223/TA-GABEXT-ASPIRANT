67f387a39c1bee8b731a9e76d0bb7fc3d631caff
AfitSantoso <afitdesantos@gmail.com>
2022-12-26 19:31:16 +0700

    Afit:Buat halaman Home

---
+++ afit/client/src/components/index.js
@@ -0,0 +1,33 @@
+import FeaturedProducts from './FeaturedProducts/';
+import Navbar from './Navbar/';
+import CartButtons from './CartButtons//';
+import Sidebar from './Sidebar/';
+import Hero from './Hero/';
+import Services from './Services/';
+import Contact from './Contact/';
+import AddToCart from './AddToCart/';
+import PageHero from './PageHero/';
+import Toast from './Toast/';
+import ErrorBoundary from './ErrorBoundary/';
+import Footer from './Footer/';
+import AmountButtons from './AmountButtons/';
+import Button from './Button/';
+import Loading from './Loading/';
+
+export {
+  FeaturedProducts,
+  Navbar,
+  CartButtons,
+  Sidebar,
+  Hero,
+  Services,
+  Contact,
+  AddToCart,
+  PageHero,
+  Toast,
+  Footer,
+  AmountButtons,
+  Button,
+  Loading,
+  ErrorBoundary,
+};
